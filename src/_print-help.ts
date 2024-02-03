ca3d230b6a9cac18efd6ac895461bae7a85cf78c
artze <arthurwtm@gmail.com>
2024-02-03 10:58:47 +0800

    Add description in help

--- src/_print-help.ts
+++ src/_print-help.ts
@@ -1,5 +1,14 @@
 export function printHelp() {
-  console.log(`Usage: denoinit [OPTIONS...]`);
+  console.log(
+    "%cdenoinit%c creates the following files:",
+    "font-weight: bold",
+    "font-weight: normal"
+  );
+  console.log("  * vscode workspace settings to enable deno plugin");
+  console.log("  * deps.ts file");
+  console.log("  * entrypoint file");
+  console.log("  * deno.json with sensible defaults");
+  console.log("\nUsage: denoinit [OPTIONS...]");
   console.log("\nOptional flags:");
   console.log("  -h, --help                Display this help and exit");
   console.log(
